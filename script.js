70514aecbcaa729b9cb07be6dcbe22e10fcf3d4a
octronnnnnn <108590339+octronnnnnn@users.noreply.github.com>
2023-11-23 17:04:11 +0100

    added copy email button

--- script.js
+++ script.js
@@ -11,5 +11,17 @@ window.addEventListener("scroll", () => {
     navUl.classList.toggle("sticky", window.scrollY > 0);
  })
 
+//copy email to clipboard
+const email = document.querySelector("#email").innerHTML;
+const copyContent = async () => {
+    try {
+        await navigator.clipboard.writeText(email);
+        let copyBtn = document.querySelector("#copyBtn");
+        console.log("Content copied to clipboard");
+        copyBtn.textContent = "Copied!";
+    } catch (error) {
+        console.log("Failed to copy: ", error);
+    }
+}
 
- 
+document.getElementById("copyBtn").addEventListener("click", copyContent);
