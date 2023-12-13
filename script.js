3505eeb21586a553207039d4e227ea5e14116744
octronnnnnn <108590339+octronnnnnn@users.noreply.github.com>
2023-12-13 18:33:41 +0100

    rebuild

--- script.js
+++ script.js
@@ -1,25 +1,5 @@
-document.addEventListener("DOMContentLoaded", () => {
 // nav bar animation
 window.addEventListener("scroll", () => {
     const navBar = document.querySelector("nav");
     navBar.classList.toggle("sticky", window.scrollY > 0); 
-    // show home element only when scrolled 
-    const home = document.querySelector("#home");
-    home.classList.toggle("show", window.scrollY > 0);
- })
-
-//copy email to clipboard
-const email = document.querySelector("#email").innerHTML;
-const copyContent = async () => {
-    try {
-        await navigator.clipboard.writeText(email);
-        let copyBtn = document.querySelector("#copyBtn");
-        console.log("Content copied to clipboard");
-        copyBtn.textContent = "Copied!";
-    } catch (error) {
-        console.log("Failed to copy: ", error);
-    }
-}
-
-document.getElementById("copyBtn").addEventListener("click", copyContent);
 })
